000ca2a7ce449d9be21cc688ac7c998755e2c5d7
ruriko <44691657+ruriko123@users.noreply.github.com>
2023-01-15 22:33:44 +0545

    Update background.js

--- background.js
+++ background.js
@@ -7,18 +7,6 @@ const create = async() => {
     await ffmpeg.load();
     return ffmpeg;
 };
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-
-
-
 
 const get_reddit = async(link) => {
     try {
