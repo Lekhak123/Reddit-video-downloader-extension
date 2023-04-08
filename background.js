88975e5eb796d1f777917eee335a8e0a90fc9da7
ruriko <44691657+ruriko123@users.noreply.github.com>
2023-04-08 21:37:56 +0545

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,8 @@
-const create = async() => {
-    let ffmpeg = undefined;
-    const settings = {
-        corePath: chrome.runtime.getURL("/src/vendor/ffmpeg-core.js"),
-    };
-    ffmpeg = await FFmpeg.createFFmpeg(settings);
-    await ffmpeg.load();
-    return ffmpeg;
-};
+// const create = async() => {     try {         let ffmpeg = undefined;
+// const settings = {             corePath: chrome                 .runtime
+//            .getURL("/src/vendor/ffmpeg-core.js")         };         ffmpeg =
+// await FFmpeg.createFFmpeg(settings);         await ffmpeg.load();
+// return ffmpeg;     } catch (error) {} };
 
 const get_reddit = async(link) => {
     try {
@@ -16,92 +12,128 @@ const get_reddit = async(link) => {
             ?.children[0].data
                 ?.secure_media
                     ?.reddit_video
-                        ?.fallback_url
-        let normal_url = audio_url?.replace("?source=fallback", "")
-        audio_url = audio_url?.replace(/DASH_[0-9]+/gm, "DASH_audio")
-        audio_url = audio_url?.replace("?source=fallback", "")
+                        ?.fallback_url;
+        let normal_url = audio_url
+            ?.replace("?source=fallback", "");
+        audio_url = audio_url
+            ?.replace(/DASH_[0-9]+/gm, "DASH_audio");
+        audio_url = audio_url
+            ?.replace("?source=fallback", "");
         if (!audio_url) {
-            return "Error"
-        }
+            return "Error";
+        };
         return [normal_url, audio_url]
     } catch (error) {
-        return "Error"
+        return "Error";
     }
 
 }
 
-// window.onload = function(event){
-//     chrome.tabs.onUpdated.addListener(function () {
-//         chrome.contextMenus.create({
-//             "documentUrlPatterns": ["*://*.reddit.com/r/*/comments/*/*/"],
-//             title: "Download and save the video", id: "menu1", contexts: ["video"]});
-//     });
+// window.onload = function(event){ chrome.tabs.onUpdated.addListener(function
+// () { chrome.contextMenus.create({             "documentUrlPatterns":
+// ["*://*.reddit.com/r/*/comments/*/*/"],       title: "Download and save the
+// //* video", id: "menu1", contexts: ["video"]});     }); }
 
-// }
-
-
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.contextMenus.create({
-        "documentUrlPatterns": ["*://*.reddit.com/r/*/comments/*/*/"],
-        title: "Download and save the video", id: "menu1", contexts: ["video"]});
-});
+chrome
+    .runtime
+    .onInstalled
+    .addListener(function () {
+        chrome
+            .contextMenus
+            .create({"documentUrlPatterns": ["*://*.reddit.com/r/*/comments/*/*/"], title: "Download and save the video", id: "menu1", contexts: ["video"]});
+    });
 
 async function saveAs(blob, fileName) {
-    var url = window.URL.createObjectURL(blob);
-    var anchorElem = document.createElement("a");
-    anchorElem.style = "display: none";
-    anchorElem.href = url;
-    anchorElem.download = fileName;
 
-    document.body.appendChild(anchorElem);
-    anchorElem.click();
+    try {
+        var url = window
+            .URL
+            .createObjectURL(blob);
+        var anchorElem = document.createElement("a");
+        anchorElem.style = "display: none";
+        anchorElem.href = url;
+        anchorElem.download = fileName;
 
-    document.body.removeChild(anchorElem);
+        document
+            .body
+            .appendChild(anchorElem);
+        anchorElem.click();
 
-    setTimeout(async function() {
-        window.URL.revokeObjectURL(url);
-    
-    
-        chrome.contextMenus.update('menu1', {
-            title: "Download and save the video",
-            enabled: true
-        });
+        document
+            .body
+            .removeChild(anchorElem);
 
-        setTimeout(() => {
-            //chrome.tabs.reload();
+        setTimeout(async function () {
+            window
+                .URL
+                .revokeObjectURL(url);
 
-            chrome.runtime.reload();
+            chrome
+                .contextMenus
+                .update('menu1', {
+                    title: "Download and save the video",
+                    enabled: true
+                });
 
-        }, 3000);
-        
+            setTimeout(() => {
+                //chrome.tabs.reload();
 
-    }, 1000);
+                chrome
+                    .runtime
+                    .reload();
+
+            }, 3000);
+
+        }, 1000);
+    } catch (error) {}
+
 }
 
-chrome.contextMenus.onClicked.addListener(async function (info, tab) {
-    chrome.contextMenus.update('menu1', {
-        title:"Downloading the video.",
-        enabled: false
-    });
-        let url_link = info.pageUrl
-        let res = await get_reddit(url_link)
-        if (res==="Error"){
-            chrome.contextMenus.update('menu1', {
-                title:"Downloading the video.",
-                enabled: true
-            });
-        } else {
-            
-        const ffmpeg = await create();
-        let { fetchFile } = FFmpeg;
-        ffmpeg.FS('writeFile', 'video.mp4', await fetchFile(res[0]));
-        ffmpeg.FS('writeFile', 'audio.mp4', await fetchFile(res[1]));
-        await ffmpeg.run('-i', 'video.mp4', '-i', 'audio.mp4', '-c', 'copy', 'output.mp4');
-        let data = await ffmpeg.FS('readFile', 'output.mp4');
-        let byteArray  = new Uint8Array(data.buffer);
-        var blob1 = new Blob([byteArray], {type: "application/octet-stream"});
-        var fileName1 = "killme.mp4";
-        await saveAs(blob1, fileName1)
-}
-        
+chrome
+    .contextMenus
+    .onClicked
+    .addListener(async function (info, tab) {
+        try {
+
+            chrome
+                .contextMenus
+                .update('menu1', {
+                    title: "Downloading the video.",
+                    enabled: false
+                });
+            let url_link = info.pageUrl;
+            let res = await get_reddit(url_link);
+            if (res === "Error") {
+                chrome
+                    .contextMenus
+                    .update('menu1', {
+                        title: "Downloading the video.",
+                        enabled: true
+                    });
+            } else {
+
+                let ffmpeg = undefined;
+                const settings = {
+                    corePath: chrome
+                        .runtime
+                        .getURL("/src/vendor/ffmpeg-core.js")
+                };
+                ffmpeg = await FFmpeg.createFFmpeg(settings);
+                await ffmpeg.load();
+
+                let {fetchFile} = FFmpeg;
+                ffmpeg.FS('writeFile', 'video.mp4', await fetchFile(res[0]));
+                ffmpeg.FS('writeFile', 'audio.mp4', await fetchFile(res[1]));
+                await ffmpeg.run('-i', 'video.mp4', '-i', 'audio.mp4', '-c', 'copy', 'output.mp4');
+                let data = await ffmpeg.FS('readFile', 'output.mp4');
+                let byteArray = new Uint8Array(data.buffer);
+                var blob1 = new Blob([byteArray], {type: "application/octet-stream"});
+                var fileName1 = "killme.mp4";
+                await saveAs(blob1, fileName1);
+                setTimeout(() => {
+                    ffmpeg.exit(); // ffmpeg.exit() is callable only after load() stage.
+                }, 100);
+            }
+        } catch (error) {}
+
     });
