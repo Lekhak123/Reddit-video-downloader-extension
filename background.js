0746a7202fff6bf35631114216d2ea123d4f124e
ruriko123 <hriayesh.lekhak@gmail.com>
2023-09-11 20:42:55 -0500

    Fixed the breaking changes with reddit api and refactored the code

--- background.js
+++ background.js
@@ -1,13 +1,27 @@
-// const create = async() => {     try {         let ffmpeg = undefined;
-// const settings = {             corePath: chrome                 .runtime
-//            .getURL("/src/vendor/ffmpeg-core.js")         };         ffmpeg =
-// await FFmpeg.createFFmpeg(settings);         await ffmpeg.load();
-// return ffmpeg;     } catch (error) {} };
+/* Enable/Disable the context menu*/
+const enableContextMenu = (Menustatus) => {
+    if (Menustatus) {
+        return chrome
+            .contextMenus
+            .update('Reddit_video_downloader', {
+                title: "Download",
+                enabled: true
+            });
+    } else {
+        return chrome
+            .contextMenus
+            .update('Reddit_video_downloader', {
+                title: "Downloading the video.",
+                enabled: false
+            });
+    };
+};
 
+/* Get the reddit video and audio link from the video post url */
 const get_reddit = async(link) => {
     try {
-        const response = await fetch(link + ".json");
-        const data = await response.json();
+        let response = await fetch(`${link}.json`);
+        let data = await response.json();
         let audio_url = data[0].data
             ?.children[0].data
                 ?.secure_media
@@ -16,124 +30,161 @@ const get_reddit = async(link) => {
         let normal_url = audio_url
             ?.replace("?source=fallback", "");
         audio_url = audio_url
-            ?.replace(/DASH_[0-9]+/gm, "DASH_audio");
+            ?.replace(/DASH_[0-9]+/gm, "DASH_AUDIO_128");
         audio_url = audio_url
             ?.replace("?source=fallback", "");
-        if (!audio_url) {
-            return "Error";
-        };
+        response,
+        data = undefined;
         return [normal_url, audio_url]
     } catch (error) {
-        return "Error";
-    }
+        throw new Error("Error occured while downloading the reddit video.");
+    };
+};
 
-}
+var contextMenuItem = {
+    documentUrlPatterns: ["*://*.reddit.com/r/*/comments/*/*/"],
+    title: "Download",
+    id: "Reddit_video_downloader",
+    contexts: ["video"]
+};
 
-// window.onload = function(event){ chrome.tabs.onUpdated.addListener(function
-// () { chrome.contextMenus.create({             "documentUrlPatterns":
-// ["*://*.reddit.com/r/*/comments/*/*/"],       title: "Download and save the
-// //* video", id: "menu1", contexts: ["video"]});     }); }
-
+/* Initialize the context menu */
 chrome
     .runtime
     .onInstalled
     .addListener(function () {
         chrome
             .contextMenus
-            .create({"documentUrlPatterns": ["*://*.reddit.com/r/*/comments/*/*/"], title: "Download and save the video", id: "menu1", contexts: ["video"]});
+            .create(contextMenuItem)
     });
 
+/* Download the reddit video after processing is complete */
 async function saveAs(blob, fileName) {
-
     try {
-        var url = window
+        let url = window
             .URL
             .createObjectURL(blob);
-        var anchorElem = document.createElement("a");
+        let anchorElem = document.createElement("a");
         anchorElem.style = "display: none";
         anchorElem.href = url;
         anchorElem.download = fileName;
-
         document
             .body
             .appendChild(anchorElem);
         anchorElem.click();
-
         document
             .body
             .removeChild(anchorElem);
+        anchorElem,
+        url = undefined;
+    } catch (error) {
+        throw new Error("Error occured while saving the video.");
+    };
+};
 
-        setTimeout(async function () {
-            window
-                .URL
-                .revokeObjectURL(url);
+var ffmpegObject = {
+    ffmpeg: undefined,
+    fetchFile: undefined,
+    chromeObject: undefined
+};
 
-            chrome
-                .contextMenus
-                .update('menu1', {
-                    title: "Download and save the video",
-                    enabled: true
-                });
+const saveChromeObject = async(chrome) => {
+    ffmpegObject.chromeObject = chrome;
+    return;
+};
 
-            setTimeout(() => {
-                //chrome.tabs.reload();
+/* Initialize ffmpeg and save it to ffmpegObject */
+const initializeFFmpeg = async() => {
+    if (!(ffmpegObject.chromeObject)) {
+        console.log("herere")
+        throw new Error("Chrome object not initialized.");
+    };
+    const settings = {
+        corePath: chrome
+            .runtime
+            .getURL('/src/vendor/ffmpeg-core.js')
+    };
+    ffmpegObject.ffmpeg = await FFmpeg.createFFmpeg(settings);
+    await ffmpegObject
+        .ffmpeg
+        .load();
+    let {fetchFile} = FFmpeg;
+    ffmpegObject.fetchFile = fetchFile; // Assign the fetchFile function
+    return;
+};
 
-                chrome
-                    .runtime
-                    .reload();
+/* Remove the recently saved raw video, audio, and output video from ffmpeg memory */
+const clearFFmpegMemory = () => {
+    ffmpegObject
+        .ffmpeg
+        .FS
+        .unlink('video.mp4');
+    ffmpegObject
+        .ffmpeg
+        .FS
+        .unlink('audio.mp4');
+    ffmpegObject
+        .ffmpeg
+        .FS
+        .unlink('output.mp4');
+    return;
+};
 
-            }, 3000);
-
-        }, 1000);
-    } catch (error) {}
-
-}
+/* Generate the output video from the raw video and audio using ffmpeg */
+const processVideo = async(videomp4, audiomp4, fetchFile) => {
+    try {
+        ffmpegObject
+            .ffmpeg
+            .FS('writeFile', 'video.mp4', await fetchFile(videomp4));
+        ffmpegObject
+            .ffmpeg
+            .FS('writeFile', 'audio.mp4', await fetchFile(audiomp4));
+        await ffmpegObject
+            .ffmpeg
+            .run('-i', 'video.mp4', '-i', 'audio.mp4', '-c', 'copy', 'output.mp4');
+        let data = await ffmpegObject
+            .ffmpeg
+            .FS('readFile', 'output.mp4');
+        let byteArray = new Uint8Array(data.buffer);
+        let blob1 = new Blob([byteArray], {type: "application/octet-stream"});
+        let fileName1 = "download.mp4";
+        saveAs(blob1, fileName1);
+        data,
+        byteArray,
+        blob1 = undefined;
+        return;
+    } catch (error) {
+        throw new Error("Error occured while processing the video.");
+    };
+};
 
 chrome
     .contextMenus
     .onClicked
-    .addListener(async function (info, tab) {
+    .addListener(async function (info) {
+        if (!(info
+            ?.menuItemId == "Reddit_video_downloader")) {
+            return;
+        };
         try {
-
-            chrome
-                .contextMenus
-                .update('menu1', {
-                    title: "Downloading the video.",
-                    enabled: false
-                });
+            if (!(ffmpegObject
+                ?.chromeObject)) {
+                saveChromeObject(chrome);
+            };
+            if (!(ffmpegObject.ffmpeg)) {
+                await initializeFFmpeg();
+            };
+            enableContextMenu(false);
             let url_link = info.pageUrl;
             let res = await get_reddit(url_link);
-            if (res === "Error") {
-                chrome
-                    .contextMenus
-                    .update('menu1', {
-                        title: "Downloading the video.",
-                        enabled: true
-                    });
-            } else {
-
-                let ffmpeg = undefined;
-                const settings = {
-                    corePath: chrome
-                        .runtime
-                        .getURL("/src/vendor/ffmpeg-core.js")
-                };
-                ffmpeg = await FFmpeg.createFFmpeg(settings);
-                await ffmpeg.load();
-
-                let {fetchFile} = FFmpeg;
-                ffmpeg.FS('writeFile', 'video.mp4', await fetchFile(res[0]));
-                ffmpeg.FS('writeFile', 'audio.mp4', await fetchFile(res[1]));
-                await ffmpeg.run('-i', 'video.mp4', '-i', 'audio.mp4', '-c', 'copy', 'output.mp4');
-                let data = await ffmpeg.FS('readFile', 'output.mp4');
-                let byteArray = new Uint8Array(data.buffer);
-                var blob1 = new Blob([byteArray], {type: "application/octet-stream"});
-                var fileName1 = "download.mp4";
-                await saveAs(blob1, fileName1);
-                setTimeout(() => {
-                    ffmpeg.exit(); // ffmpeg.exit() is callable only after load() stage.
-                }, 100);
-            }
-        } catch (error) {}
-
+            let fetchFile = ffmpegObject
+                ?.fetchFile;
+            await processVideo(res[0], res[1], fetchFile);
+            url_link,
+            res = undefined;
+            enableContextMenu(true);
+            clearFFmpegMemory();
+        } catch (error) {
+            enableContextMenu(true);
+        };
     });
